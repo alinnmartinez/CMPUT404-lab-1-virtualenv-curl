669ea51055513d783a33489aafd9f87b152d5df5
alinnmartinez <alinn1@ualberta.ca>
2023-01-13 00:11:57 -0800

    downloads and runs itself

--- python_script.py
+++ python_script.py
@@ -5,3 +5,11 @@ print(requests.__version__)
 
 # getting Google homepage
 response = requests.get('https://www.google.com')
+
+# downloads the file and runs itself     referenced: Mokhtar Ebrahim https://likegeeks.com/downloading-files-using-python/
+script = requests.get('https://raw.githubusercontent.com/alinnmartinez/CMPUT404-lab-1-virtualenv-curl/main/python_script.py')
+
+open('./script_to_read.py', 'wb').write(script.content)
+
+file = open('./script_to_read.py', 'r')
+print(file.read())
